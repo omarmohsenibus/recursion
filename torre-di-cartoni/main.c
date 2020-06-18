f3b3b34ca6a13d4325f4d6b19433f71c6b3ade25
Utente ospite <Guest@MacBook-Air-di-mac.local>
2020-06-18 16:18:18 +0200

    add project torre-di-cartone

---
+++ torre-di-cartoni/main.c
@@ -0,0 +1,13 @@
+#include "torrecartoni.h"
+int main(void){
+    
+    cartone c[] = {
+        { 3, 2, 5 },
+        { 2, 5, 6 },
+        { 1, 5, 2 }
+    };
+    int n = 3;
+
+    TorreCartoni(c, n);
+    return 0;
+}
